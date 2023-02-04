1d5ce35d5815993e4008bc0ba5a996dbd795917c
Parthasarathy <sarathi475@gmail.com>
2023-02-04 20:36:06 +0530

    notification added

---
+++ lib/home.dart
@@ -0,0 +1,192 @@
+import 'package:awesome_notifications/awesome_notifications.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_local_notification_sample/notification.dart';
+
+///  *********************************************
+///     HOME PAGE
+///  *********************************************
+///
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: const <Widget>[
+            Text(
+              'Push the buttons below to create new notifications',
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: Padding(
+        padding: const EdgeInsets.all(20.0),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const SizedBox(width: 20),
+            FloatingActionButton(
+              heroTag: '1',
+              onPressed: () => NotificationController.createNewNotification(),
+              tooltip: 'Create New notification',
+              child: const Icon(Icons.outgoing_mail),
+            ),
+            const SizedBox(width: 10),
+            FloatingActionButton(
+              heroTag: '2',
+              onPressed: () => NotificationController.scheduleNewNotification(),
+              tooltip: 'Schedule New notification',
+              child: const Icon(Icons.access_time_outlined),
+            ),
+            const SizedBox(width: 10),
+            FloatingActionButton(
+              heroTag: '3',
+              onPressed: () => NotificationController.resetBadgeCounter(),
+              tooltip: 'Reset badge counter',
+              child: const Icon(Icons.exposure_zero),
+            ),
+            const SizedBox(width: 10),
+            FloatingActionButton(
+              heroTag: '4',
+              onPressed: () => NotificationController.cancelNotifications(),
+              tooltip: 'Cancel all notifications',
+              child: const Icon(Icons.delete_forever),
+            ),
+          ],
+        ),
+      ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+}
+
+///  *********************************************
+///     NOTIFICATION PAGE
+///  *********************************************
+///
+class NotificationPage extends StatelessWidget {
+  const NotificationPage({Key? key, required this.receivedAction})
+      : super(key: key);
+
+  final ReceivedAction receivedAction;
+
+  @override
+  Widget build(BuildContext context) {
+    bool hasLargeIcon = receivedAction.largeIconImage != null;
+    bool hasBigPicture = receivedAction.bigPictureImage != null;
+    double bigPictureSize = MediaQuery.of(context).size.height * .4;
+    double largeIconSize =
+        MediaQuery.of(context).size.height * (hasBigPicture ? .12 : .2);
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(receivedAction.title ?? receivedAction.body ?? ''),
+      ),
+      body: SingleChildScrollView(
+        padding: EdgeInsets.zero,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            SizedBox(
+                height:
+                    hasBigPicture ? bigPictureSize + 40 : largeIconSize + 60,
+                child: hasBigPicture
+                    ? Stack(
+                        children: [
+                          if (hasBigPicture)
+                            FadeInImage(
+                              placeholder: const NetworkImage(
+                                  'https://cdn.syncfusion.com/content/images/common/placeholder.gif'),
+                              //AssetImage('assets/images/placeholder.gif'),
+                              height: bigPictureSize,
+                              width: MediaQuery.of(context).size.width,
+                              image: receivedAction.bigPictureImage!,
+                              fit: BoxFit.cover,
+                            ),
+                          if (hasLargeIcon)
+                            Positioned(
+                              bottom: 15,
+                              left: 20,
+                              child: ClipRRect(
+                                borderRadius: BorderRadius.all(
+                                    Radius.circular(largeIconSize)),
+                                child: FadeInImage(
+                                  placeholder: const NetworkImage(
+                                      'https://cdn.syncfusion.com/content/images/common/placeholder.gif'),
+                                  //AssetImage('assets/images/placeholder.gif'),
+                                  height: largeIconSize,
+                                  width: largeIconSize,
+                                  image: receivedAction.largeIconImage!,
+                                  fit: BoxFit.cover,
+                                ),
+                              ),
+                            )
+                        ],
+                      )
+                    : Center(
+                        child: ClipRRect(
+                          borderRadius:
+                              BorderRadius.all(Radius.circular(largeIconSize)),
+                          child: FadeInImage(
+                            placeholder: const NetworkImage(
+                                'https://cdn.syncfusion.com/content/images/common/placeholder.gif'),
+                            //AssetImage('assets/images/placeholder.gif'),
+                            height: largeIconSize,
+                            width: largeIconSize,
+                            image: receivedAction.largeIconImage!,
+                            fit: BoxFit.cover,
+                          ),
+                        ),
+                      )),
+            Padding(
+              padding: const EdgeInsets.only(bottom: 20.0, left: 20, right: 20),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.start,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  RichText(
+                      text: TextSpan(children: [
+                    if (receivedAction.title?.isNotEmpty ?? false)
+                      TextSpan(
+                        text: receivedAction.title!,
+                        style: Theme.of(context).textTheme.titleLarge,
+                      ),
+                    if ((receivedAction.title?.isNotEmpty ?? false) &&
+                        (receivedAction.body?.isNotEmpty ?? false))
+                      TextSpan(
+                        text: '\n\n',
+                        style: Theme.of(context).textTheme.bodyMedium,
+                      ),
+                    if (receivedAction.body?.isNotEmpty ?? false)
+                      TextSpan(
+                        text: receivedAction.body!,
+                        style: Theme.of(context).textTheme.bodyMedium,
+                      ),
+                  ]))
+                ],
+              ),
+            ),
+            Container(
+              color: Colors.black12,
+              padding: const EdgeInsets.all(20),
+              width: MediaQuery.of(context).size.width,
+              child: Text(receivedAction.toString()),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,77 @@
+import 'package:flutter/material.dart';
+import 'package:awesome_notifications/awesome_notifications.dart';
+import 'package:flutter_local_notification_sample/home.dart';
+import 'package:flutter_local_notification_sample/notification.dart';
+
+Future<void> main() async {
+  // Always initialize Awesome Notifications
+  await NotificationController.initializeLocalNotifications();
+  runApp(const MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  const MyApp({super.key});
+
+  // The navigator key is necessary to navigate using static methods
+  static final GlobalKey<NavigatorState> navigatorKey =
+      GlobalKey<NavigatorState>();
+
+  static Color mainColor = const Color(0xFF9D50DD);
+
+  @override
+  State<MyApp> createState() => _AppState();
+}
+
+class _AppState extends State<MyApp> {
+  // This widget is the root of your application.
+
+  static const String routeHome = '/', routeNotification = '/notification-page';
+
+  @override
+  void initState() {
+    NotificationController.startListeningNotificationEvents();
+    super.initState();
+  }
+
+  List<Route<dynamic>> onGenerateInitialRoutes(String initialRouteName) {
+    List<Route<dynamic>> pageStack = [];
+    pageStack.add(MaterialPageRoute(
+        builder: (_) =>
+            const MyHomePage(title: 'Flutter Notifications sample')));
+    if (initialRouteName == routeNotification &&
+        NotificationController.initialAction != null) {
+      pageStack.add(MaterialPageRoute(
+          builder: (_) => NotificationPage(
+              receivedAction: NotificationController.initialAction!)));
+    }
+    return pageStack;
+  }
+
+  Route<dynamic>? onGenerateRoute(RouteSettings settings) {
+    switch (settings.name) {
+      case routeHome:
+        return MaterialPageRoute(
+            builder: (_) =>
+                const MyHomePage(title: 'Flutter Notifications sample'));
+
+      case routeNotification:
+        ReceivedAction receivedAction = settings.arguments as ReceivedAction;
+        return MaterialPageRoute(
+            builder: (_) => NotificationPage(receivedAction: receivedAction));
+    }
+    return null;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Notifications sample',
+      navigatorKey: MyApp.navigatorKey,
+      onGenerateInitialRoutes: onGenerateInitialRoutes,
+      onGenerateRoute: onGenerateRoute,
+      theme: ThemeData(
+        primarySwatch: Colors.deepPurple,
+      ),
+    );
+  }
+}
